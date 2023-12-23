20bf584040fe77a03fa57fc40a96f203bff8fd93
MysticalOS <mysticalosx@gmail.com>
2023-12-22 21:19:39 -0500

    allow fated affixes to load without category ID

--- DBM-Raids-Shadowlands/FatedAffixes.lua
+++ DBM-Raids-Shadowlands/FatedAffixes.lua
@@ -1,4 +1,4 @@
-local mod	= DBM:NewMod("FatedAffixes", "DBM-Raids-Shadowlands")
+local mod	= DBM:NewMod("FatedAffixes", "DBM-Raids-Shadowlands", 0)
 local L		= mod:GetLocalizedStrings()
 
 mod:SetRevision("@file-date-integer@")
